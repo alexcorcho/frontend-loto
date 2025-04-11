99b74fef78d92832b8e2075afe0c31b356592fc3
RobertAlonso <robertalonsocardonafernandez@gmail.com>
2025-04-11 13:33:26 -0500

    Diseño de Login

--- src/router/index.js
+++ src/router/index.js
@@ -1,13 +1,14 @@
 import { createRouter, createWebHistory } from 'vue-router'
-import HomeView from '../views/HomeView.vue'
+import DashboardView from '../views/Auth/AuthView.vue'
+
 
 const router = createRouter({
   history: createWebHistory(import.meta.env.BASE_URL),
   routes: [
     {
       path: '/',
-      name: 'home',
-      component: HomeView,
+      name: 'Login',
+      component: DashboardView,
     },
     {
       path: '/about',

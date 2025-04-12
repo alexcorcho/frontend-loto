6e06f8df71e951cd12b107986d73c7e73dc5e2a0
Alex Corcho <alexander.corcho@gmail.com>
2025-04-11 20:10:08 -0500

    Funcionalidad login

--- src/router/index.js
+++ src/router/index.js
@@ -1,22 +1,17 @@
 import { createRouter, createWebHistory } from 'vue-router'
-import DashboardView from '../views/Auth/AuthView.vue'
-
 
 const router = createRouter({
   history: createWebHistory(import.meta.env.BASE_URL),
   routes: [
     {
-      path: '/',
-      name: 'Login',
-      component: DashboardView,
+      path: '/login',
+      name: 'login',
+      component: () => import('../views/Auth/AuthView.vue'),
     },
     {
-      path: '/about',
-      name: 'about',
-      // route level code-splitting
-      // this generates a separate chunk (About.[hash].js) for this route
-      // which is lazy-loaded when the route is visited.
-      component: () => import('../views/AboutView.vue'),
+      path: '/dashboard',
+      name: 'dashboard',
+      component: () => import('../views/Admin/Profile/Dashboard/DashboardView.vue'),
     },
   ],
 })
---
+++ src/views/Admin/Profile/Dashboard/DashboardView.vue
@@ -0,0 +1,1 @@
+<template>soy el dash</template>
